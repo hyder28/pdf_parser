01d62c0f7963c653c3799c073684aa06bb29cbf6
hyder28 <hyderalis.2017@mitb.smu.edu.sg>
2022-08-09 16:48:36 +0800

    added datetime

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 from fastapi import FastAPI, UploadFile
 from utils.run_pdf_parse import get_pdf_extraction
 
+from datetime import datetime
+
 app = FastAPI()
 
 
@@ -22,7 +24,7 @@ async def extract_pdf(pdf: UploadFile) -> dict:
     Extracts pdf to parent-child relationships
     """
     logging.basicConfig(filename="parser_app.log", level=logging.DEBUG)
-    logging.info("Started")
+    logging.info(f"Started at {str(datetime.now())}")
 
     pdf_fpath = os.path.join("./pdf_files", pdf.filename)
     pdf_fname = pdf.filename[:-4]
@@ -30,10 +32,10 @@ async def extract_pdf(pdf: UploadFile) -> dict:
     df_result = get_pdf_extraction(pdf_fpath, pdf_fname)
     dict_result = df_result.to_dict(orient="records")
 
-    logging.info("Finished")
+    logging.info(f"Ended at {str(datetime.now())}")
 
     return dict_result
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, port=5000)
